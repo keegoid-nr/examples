4dc5c3f89e4c3397aff7ca75521ea02b5440771a
Keegan Mullaney <kmullaney@newrelic.com>
2023-02-28 14:51:14 -0800

    add dt code

--- python/function.py
+++ python/function.py
@@ -1,16 +1,40 @@
-# import newrelic.agent
+import newrelic.agent
+import requests
 import subprocess
 
 # In a python Lambda, the runtime loads the handler code as a module; so code in the top level
 # of the module occurs once, during cold start.
 print("Lambda Handler starting up")
 
-def lambda_handler(event, context):
-  # At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
 
-  # perform cURL operation and print results
-  cmd = ["curl", "-vvv", "https://log-api.eu.newrelic.com/log/v1/"]
-  r = subprocess.run(cmd, capture_output=True, check=True, text=True)
-  print("***STDOUT***\n", r.stdout)
-  print("***STDERR***\n", r.stderr)
-  return r.returncode
+@newrelic.agent.lambda_handler()
+def lambda_handler(event, context):
+    # At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
+
+    # Get the transaction object for this function
+    transaction = newrelic.agent.current_transaction()
+
+    # Accept distributed tracing headers
+    headers = event.get("headers", {})
+    transaction.accept_distributed_trace_headers(headers, "HTTP")
+
+    # Print out the distributed tracing headers
+    trace_headers = transaction.distributed_trace_headers()
+    print("Distributed tracing headers:")
+    for key, value in trace_headers.items():
+        print(f"{key}: {value}")
+
+    # Make an external HTTP request and inject distributed tracing headers
+    url = "https://example.com/api"
+    headers = {}
+    transaction.insert_distributed_trace_headers(headers)
+    response = requests.get(url, headers=headers)
+
+    # perform cURL operation and print results
+    cmd = ["curl", "-vvv", url]
+    r = subprocess.run(cmd, capture_output=True, check=True, text=True)
+    print("***STDOUT***\n", r.stdout)
+    print("***STDERR***\n", r.stderr)
+    print("response status code: ", response.status_code)
+
+    return r.returncode
