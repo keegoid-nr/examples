f6854a47d9f453569a529dbe03e1ca2c732ec3ad
Keegan Mullaney <kmullaney@newrelic.com>
2023-06-07 15:46:43 -0700

    finalize dt header code

--- python/function.py
+++ python/function.py
@@ -1,34 +1,39 @@
+from newrelic import agent
 import requests
 
 # In a python Lambda, the runtime loads the handler code as a module; so code in the top level
 # of the module occurs once, during cold start.
 print("Lambda Handler starting up")
 
-@newrelic.agent.profile_trace()
+@agent.profile_trace()
 def get_user():
     print("getting DB username ")
     return "Bill"
 
-@newrelic.agent.function_trace()
+@agent.function_trace()
 def get_pass():
     print("getting DB password")
     return "123456"
 
-@newrelic.agent.lambda_handler()
+@agent.lambda_handler()
 def lambda_handler(event, context):
     # At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
-    requests.get("https://example.com")
 
     # Get the transaction object for this function
-    transaction = newrelic.agent.current_transaction()
+    transaction = agent.current_transaction()
 
-    # This is an example of a custom event. `FROM MyPythonEvent SELECT *` in New Relic will find this event.
-    newrelic.agent.record_custom_event("MyPythonEvent", {
-        "zip": "zap"
-    })
+    # Accept distributed tracing headers
+    headers = event.get("headers", {})
+    if headers:
+        transaction.accept_distributed_trace_headers(headers)
+    else:
+        # Generate distributed tracing headers if headers are not provided
+        headers_list = []
+        transaction.insert_distributed_trace_headers(headers_list)
+        headers = {k: v for k, v in headers_list}
 
-    # This attribute gets added to the normal AwsLambdaInvocation event
-    newrelic.agent.add_custom_parameter('customAttribute', 'customAttributeValue')
+    # Make an external HTTP request and inject distributed trace headers
+    response = requests.get("https://example.com", headers=headers)
 
     # additional function processes
     username = get_user()
@@ -39,18 +44,11 @@ def lambda_handler(event, context):
     print("username: ",username)
     print("password: ",password)
 
-    # Accept distributed tracing headers
-    headers = event.get("headers", {})
-    transaction.accept_distributed_trace_headers(headers)
-
     # Print out the distributed tracing headers
     print("Distributed tracing headers:")
     for key, value in headers.items():
         print(f"{key}: {value}")
 
-    # Make an external HTTP request and inject distributed tracing headers
-    url = "https://example.com/api"
-    headers_list = []
-    transaction.insert_distributed_trace_headers(headers_list)
-    headers_dict = {k: v for k, v in headers_list}
-    return requests.get(url, headers=headers_dict)
+    print("The proprietary `newrelic` header can be decoded with: `pbpaste | base64 -d | jq .`")
+
+    return response.status_code
