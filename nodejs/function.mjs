bb0963c824e8fed790ea11e6713bb00b6fe3dbd3
Keegan Mullaney <kmullaney@newrelic.com>
2023-10-30 17:29:54 -0700

    refactor: nodejs for esm

--- nodejs/function.mjs
+++ nodejs/function.mjs
@@ -1,12 +1,28 @@
-import newrelic from newrelic
+import newrelic from "newrelic"
 
 // In a Node Lambda, the runtime loads the handler code as a module; so code in the top level
 // of the module occurs once, during cold start.
 console.log("Lambda Handler starting up")
 
-const lambda_handler = async (event, context) => {
+export async function handler(event, context) {
+  const wrappedHandler = newrelic.setLambdaHandler(async (event, context) => {
+    // At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
 
-  return context.logStreamName
+    // This is an example of a custom event. `FROM MyNodeEvent SELECT *` in New Relic will find this event.
+    newrelic.recordCustomEvent("MyNodeEvent", {
+      zip: "zap",
+    })
+
+    // This attribute gets added to the normal AwsLambdaInvocation event
+    newrelic.addCustomAttributes({
+      customAttribute: "customAttributeValue",
+    })
+
+    // As normal, anything you write to stdout ends up in CloudWatch
+    console.log("Hello, world")
+
+    return "Success!"
+  })
+
+  return wrappedHandler(event, context)
 }
-
-export { lambda_handler }
