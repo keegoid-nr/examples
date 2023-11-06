b4a54a987e0499a10ea0acd17ce2f9230b2dff96
Keegan Mullaney <kmullaney@newrelic.com>
2023-11-06 11:57:40 -0800

    chore: test NEW_RELIC_LABELS

--- python/function.py
+++ python/function.py
@@ -1,16 +1,20 @@
-# import newrelic.agent
-import subprocess
+from newrelic import agent
 
 # In a python Lambda, the runtime loads the handler code as a module; so code in the top level
 # of the module occurs once, during cold start.
 print("Lambda Handler starting up")
 
-def lambda_handler(event, context):
-  # At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
+def handler(event, context):
+    # At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
 
-  # perform cURL operation and print results
-  cmd = ["curl", "-vvv", "https://log-api.eu.newrelic.com/log/v1/"]
-  r = subprocess.run(cmd, capture_output=True, check=True, text=True)
-  print("***STDOUT***\n", r.stdout)
-  print("***STDERR***\n", r.stderr)
-  return r.returncode
+    # This is an example of a custom event. `FROM MyPythonEvent SELECT *` in New Relic will find this event.
+    agent.record_custom_event("MyPythonEvent", {
+        "zip": "zap"
+    })
+    # This attribute gets added to the normal AwsLambdaInvocation event
+    agent.add_custom_attribute('customAttribute', 'customAttributeValue')
+
+    # As normal, anything you write to stdout ends up in CloudWatch
+    print("Hello, world")
+
+    return "Success!"
