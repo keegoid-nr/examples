f001726509d1c925e3d4c3afb61979985f5f6257
Keegan Mullaney <kmullaney@newrelic.com>
2024-05-07 12:24:59 -0700

    updates

--- lambda/sam/esm-nodejs/deploy.sh
+++ lambda/sam/esm-nodejs/deploy.sh
@@ -9,4 +9,4 @@ echo "bucket set to ${bucket}"
 sam build --use-container
 aws s3 mb --region "${region}" "s3://${bucket}"
 sam package --region "${region}" --s3-bucket "${bucket}" --output-template-file packaged.yaml --debug
-sam deploy --template-file packaged.yaml --stack-name "${bucket}" --debug
+sam deploy --template-file packaged.yaml --stack-name "${bucket}" --capabilities CAPABILITY_IAM --parameter-overrides NRAccountId="$2" NRTrustedAccountKey="$3" SecretsManagerSecretName="$4" --debug
