970a431496c3059871bfb67b9c652fc55300a113
Keegan Mullaney <kmullaney@newrelic.com>
2024-05-09 14:14:47 -0700

    esm2 variation test

---
+++ lambda/sam/nodejs/src/esm2/function.mjs
@@ -0,0 +1,24 @@
+import newrelic from "newrelic" // in New Relic layer
+
+// In a Node Lambda, the runtime loads the handler code as a module; so code in the top level
+// of the module occurs once, during cold start.
+console.log("Lambda Handler starting up")
+
+export const handler = newrelic.setLambdaHandler(async (event) => {
+  // At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
+
+  // This is an example of a custom event. `FROM MyNodeEvent SELECT *` in New Relic will find this event.
+  newrelic.recordCustomEvent("MyNodeEvent", {
+    zip: "zap",
+  })
+
+  // This attribute gets added to the normal AwsLambdaInvocation event
+  newrelic.addCustomAttributes({
+    customAttribute: "customAttributeValue",
+  })
+
+  // As normal, anything you write to stdout ends up in CloudWatch
+  console.log("Hello, world")
+
+  return "Success!"
+})
